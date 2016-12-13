c2f697caa032d9365fa75281afd92e38a3120764
Noah Gibbs <the.codefolio.guy@gmail.com>
2016-12-13 14:27:55 -0800

    More setup and dev

--- setup.rb
+++ setup.rb
@@ -14,8 +14,25 @@ def clone_or_update_repo(repo_url, work_dir)
   else
     system("git clone #{repo_url} #{work_dir}") || raise("Couldn't 'git clone' into #{work_dir}!")
   end
+
 end
 
-clone_or_update_repo SETTINGS["discourse_git_url"], File.join(CUR_DIRECTORY, "work", "discourse")
-clone_or_update_repo SETTINGS["ruby_git_url"], File.join(CUR_DIRECTORY, "work", "ruby")
+DISCOURSE_DIR = File.join(CUR_DIRECTORY, "work", "discourse")
+RUBY_DIR = File.join(CUR_DIRECTORY, "work", "ruby")
 
+clone_or_update_repo SETTINGS["discourse_git_url"], DISCOURSE_DIR
+clone_or_update_repo SETTINGS["ruby_git_url"], RUBY_DIR
+
+system("cd #{DISCOURSE_DIR} && bundle") || raise("Failed running bundler in #{DISCOURSE_DIR}")
+
+Dir.chdir(RUBY_DIR) do
+  unless File.exists?("configure")
+    system("autoconf") || raise("Couldn't run autoconf in #{RUBY_DIR}!")
+  end
+  unless File.exists?("Makefile")
+    system("./configure") || raise("Couldn't run configure in #{RUBY_DIR}!")
+  end
+  system("make") || raise("Make failed in #{RUBY_DIR}!")
+end
+
+system "cd #{RUBY_DIR} && autoconf && ./configure && make"
---
+++ start.rb
@@ -0,0 +1,114 @@
+#!/usr/bin/env ruby
+
+# Start the Rails server and measure time to first request.
+
+# TODO: allow customizing port number
+
+require 'rest-client'
+
+def get_rails_server_pid
+  ps_out = `ps | grep -v grep | grep puma | grep MarkApp | grep 4567`
+  if ps_out.strip =~ /(\d+)/
+    $1.to_i
+  else
+    nil
+  end
+end
+
+def clean_server_for_startup
+  server_pid = get_rails_server_pid
+  if server_pid
+    puts "Existing Rails server found on port 4567, killing PID #{server_pid.inspect}."
+    Process.kill "KILL", server_pid
+  end
+
+
+end
+
+def server_start
+  # Start the server
+  fork do
+    system "cd work/discourse && RAILS_ENV=production rails server -p 4567"
+  end
+end
+
+def server_stop
+  server_pid = get_rails_server_pid
+  if server_pid
+    Process.kill("INT", server_pid)
+    puts "Interrupted Rails server at PID #{server_pid.inspect}."
+  else
+    puts "No Rails server found, not killing."
+  end
+end
+
+def single_run_benchmark_output_and_time
+  t0 = Time.now
+  loop do
+    sleep 0.01
+    output = `curl -f http://localhost:4567/benchmark/start 2>/dev/null`
+    next unless $?.success?
+    return [output, Time.now - t0]
+  end
+end
+
+def with_started_server
+  server_start
+  yield
+ensure
+  server_stop
+end
+
+def with_running_server
+  with_started_server do
+    loop do
+      sleep 0.01
+      output = `curl -f http://localhost:4567/benchmark/start 2>/dev/null`
+      next unless $?.success?
+      yield
+      return
+    end
+  end
+end
+
+def full_iteration_start_stop
+  elapsed = nil
+  with_started_server do
+    server_output, elapsed = single_run_benchmark_output_and_time
+    puts "Output:\n#{server_output}"
+  end
+  elapsed.to_f
+end
+
+def basic_iteration_get_http
+  t0 = Time.now
+  RestClient.get "http://localhost:4567/benchmark/simple_request"
+  (Time.now - t0).to_f
+end
+
+# Run actual benchmark
+clean_server_for_startup
+
+# One Burn-in Iteration
+full_iteration_start_stop
+
+startup_times = (1..5).map { full_iteration_start_stop }
+request_times = nil
+
+with_running_server do
+  request_times = (1..5).map { basic_iteration_get_http }
+end
+
+puts "===== Startup Benchmarks ====="
+puts "Longest run: #{startup_times.max}"
+puts "Shortest run: #{startup_times.min}"
+puts "Mean: #{startup_times.inject(0.0, &:+) / startup_times.size}"
+puts "Median: #{startup_times.sort[ startup_times.size / 2 ] }"
+puts "Raw times: #{startup_times.inspect}"
+
+puts "===== Startup Benchmarks ====="
+puts "Longest run: #{request_times.max}"
+puts "Shortest run: #{request_times.min}"
+puts "Mean: #{request_times.inject(0.0, &:+) / request_times.size}"
+puts "Median: #{request_times.sort[ request_times.size / 2 ] }"
+puts "Raw times: #{request_times.inspect}"
