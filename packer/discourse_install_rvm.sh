7a6fac03505f5d830c8fadcd0f28393e4640d767
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-01-26 10:41:32 -0800

    Remove needless comment

--- packer/discourse_install_rvm.sh
+++ packer/discourse_install_rvm.sh
@@ -1,5 +1,3 @@
-# Log back in to activate RVM installation
-
 rvm install 2.3.1
 rvm --default use 2.3.1 # If this error out check https://rvm.io/integration/gnome-terminal
 gem install bundler mailcatcher
