08ba3bda5a7a35e25a2eb24d1478724495d5509c
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-01-27 14:06:49 -0800

    WIP: supply current variables to setup script

--- packer/setup.rb
+++ packer/setup.rb
@@ -16,7 +16,9 @@ def clone_or_update_repo(repo_url, work_dir)
       system("git pull") || raise("Couldn't 'git pull' in #{work_dir}!")
     end
   else
-    system("git clone #{repo_url} #{work_dir}") || raise("Couldn't 'git clone' into #{work_dir}!")
+    cmd = "git clone #{repo_url} #{work_dir}"
+    puts "Command: #{cmd.inspect}"
+    system(cmd) || raise("Couldn't 'git clone' into #{work_dir}!")
   end
 
 end
