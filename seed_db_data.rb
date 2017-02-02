9c398bb44c233622b0b7916c4df06a3d8fa4b99d
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-02-02 09:57:03 -0800

    Add comment - why is PostCreator so slow? Can we route around it (I assume)?

--- seed_db_data.rb
+++ seed_db_data.rb
@@ -102,8 +102,8 @@ topic_ids = 100.times.map do
 end
 
 puts
-puts "creating 2000 replies"
-2000.times do
+puts "creating 500 replies"  # PostCreator is just crazy slow. Why?
+500.times do
   putc "."
   PostCreator.create(users.sample, raw: sentence, topic_id: topic_ids.sample, skip_validations: true)
 end
