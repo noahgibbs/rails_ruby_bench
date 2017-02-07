3bcbbe8a20b77605673bfbae5dced2d4e1875979
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-02-07 14:44:22 -0800

    Make DiscourseClient instanced, not singleton, to allow multiple in multiple threads.

--- user_simulator.rb
+++ user_simulator.rb
@@ -53,7 +53,8 @@ end.parse!
 PORT_NUM = port_num
 
 # We want our script to generate a consistent output, so
-# we monkeypatch Array#sample to use our RNG.
+# we monkeypatch Array#sample to use our RNG. That's what
+# Gabbler uses.
 RNG = Random.new(random_seed)
 class Array
   def sample
@@ -75,21 +76,25 @@ def sentence
   sentence
 end
 
+ACTIONS = [:read_topic, :post_reply, :post_topic, :get_latest]  # Not active: :save_draft, :delete_reply. See below.
+
 class DiscourseClient
-  @cookies = nil
-  @csrf = nil
-  @prefix = "http://localhost:#{PORT_NUM}"
+  def initialize
+    @cookies = nil
+    @csrf = nil
+    @prefix = "http://localhost:#{PORT_NUM}"
 
-  @last_topics = Topic.order('id desc').limit(10).pluck(:id)
-  @last_posts = Post.order('id desc').limit(10).pluck(:id)
+    @last_topics = Topic.order('id desc').limit(10).pluck(:id)
+    @last_posts = Post.order('id desc').limit(10).pluck(:id)
+  end
 
-  def self.get_csrf_token
+  def get_csrf_token
     resp = RestClient.get "#{@prefix}/session/csrf.json"
     @cookies = resp.cookies
     @csrf = JSON.parse(resp.body)["csrf"]
   end
 
-  def self.request(method, url, payload = nil)
+  def request(method, url, payload = nil)
     args = { :method => method, :url => "#{@prefix}#{url}", :cookies => @cookies, :headers => { "X-CSRF-Token" => @csrf } }
     args[:payload] = payload if payload
     begin
@@ -104,28 +109,28 @@ class DiscourseClient
   # Given the randomized parameters for an action, take that action.
   # See below for randomized parameter generation from the random
   # seed.
-  def self.action_from_args(action_type, text, fp)
+  def action_from_args(action_type, text, fp)
     case action_type
     when :read_topic
       # Read Topic
       topic_id = @last_topics[-1]
-      DiscourseClient.request(:get, "/t/#{topic_id}.json?track_visit=true&forceLoad=true")
+      request(:get, "/t/#{topic_id}.json?track_visit=true&forceLoad=true")
     when :save_draft
       # Save draft - currently not active, need to fix 403. Wrong topic ID?
       topic_id = @last_topics[-1]
       post_id = @last_posts[-1]  # Not fully correct
       draft_hash = { "reply" => text * 5, "action" => "edit", "title" => "Title of draft reply", "categoryId" => 11, "postId" => post_id, "archetypeId" => "regular", "metaData" => nil, "sequence" => 0 }
-      DiscourseClient.request(:post, "/draft.json", "draft_key" => "topic_#{topic_id}", "data" => draft_hash.to_json)
+      request(:post, "/draft.json", "draft_key" => "topic_#{topic_id}", "data" => draft_hash.to_json)
     when :post_reply
       # Post reply
-      DiscourseClient.request(:post, "/posts", "raw" => text * 5, "unlist_topic" => "false", "category" => "9", "topic_id" => topic_id, "is_warning" => "false", "archetype" => "regular", "typing_during_msecs" => "2900", "composer_open_duration_msecs" => "12114", "featured_link" => "", "nested_post" => "true")
-      # TODO: DiscourseClient.request(:delete, "/draft.json", "draft_key" => "topic_XX", "sequence" => "0")
+      request(:post, "/posts", "raw" => text * 5, "unlist_topic" => "false", "category" => "9", "topic_id" => topic_id, "is_warning" => "false", "archetype" => "regular", "typing_during_msecs" => "2900", "composer_open_duration_msecs" => "12114", "featured_link" => "", "nested_post" => "true")
+      # TODO: request(:delete, "/draft.json", "draft_key" => "topic_XX", "sequence" => "0")
       # TODO: update @last_posts
     when :post_topic
       # Post new topic
-      DiscourseClient.request(:post, "/posts", "raw" => "", "title" => text, "unlist_topic" => "false", "category" => "", "is_warning" => "false", "archetype" => "regular", "typing_duration_msecs" => "6300", "composer_open_duration_msecs" => "31885", "nested_post" => "true")
-      # TODO: DiscourseClient.request(:delete, "/draft.json", "topic_id" => "topic_XX")
-      # TODO: DiscourseClient.request(:get, "/t/#{topic_id}.json?track_visit=true&forceLoad=true")
+      request(:post, "/posts", "raw" => "", "title" => text, "unlist_topic" => "false", "category" => "", "is_warning" => "false", "archetype" => "regular", "typing_duration_msecs" => "6300", "composer_open_duration_msecs" => "31885", "nested_post" => "true")
+      # TODO: request(:delete, "/draft.json", "topic_id" => "topic_XX")
+      # TODO: request(:get, "/t/#{topic_id}.json?track_visit=true&forceLoad=true")
       # TODO: update @last_topics
 =begin
 Started GET "/composer_messages?composer_action=createTopic&_=1483481672874" for ::1 at 2017-01-03 14:39:19 -0800
@@ -151,12 +156,12 @@ Completed 200 OK in 8ms (Views: 0.2ms | ActiveRecord: 1.4ms)
 =end
     when :delete_reply
       # Delete reply, currently not active, need to get correct Post ID
-      DiscourseClient.request(:delete, "/posts/#{post_num}")
-      DiscourseClient.request(:get, "/posts/#{post_num - 1}")
+      request(:delete, "/posts/#{post_num}")
+      request(:get, "/posts/#{post_num - 1}")
       # TODO: update @last_posts
     when :get_latest
       # Get latest
-      DiscourseClient.request(:get, "/latest.json?order=default")
+      request(:get, "/latest.json?order=default")
     else
       raise "Something is wrong! Illegal value: #{action_type}"
     end
@@ -167,25 +172,6 @@ def log(s)
   print "[#{Process.pid}]: #{s}\n"
 end
 
-user = User.offset(user_offset).first
-unless user
-  print "No user at offset #{user_offset.inspect}! Exiting.\n"
-  exit -1
-end
-
-log "Simulating activity for user id #{user.id}: #{user.name}"
-
-log "Getting Rails CSRF token..."
-DiscourseClient.get_csrf_token
-
-log "Logging in as #{user.username.inspect}..."
-DiscourseClient.request :post, "/session", { "login" => user.username, "password" => "password" }
-DiscourseClient.request :post, "/login", { "login" => user.username, "password" => "password", "redirect" => "http://localhost:#{PORT_NUM}/" }
-
-# TODO: fix number of actions
-ACTIONS = [:read_topic, :post_reply, :post_topic, :get_latest]  # Not active: :save_draft, :delete_reply. See below.
-ACTION_TYPES = ACTIONS.size
-
 # Randomize which action(s) to take, and randomize topic and reply
 # data, plus a random number for offsets.  Since we don't randomize
 # again after this, the random seed's effect is limited to this line
@@ -195,6 +181,22 @@ ACTION_TYPES = ACTIONS.size
 # and a floating-point argument (not always used.)
 actions = (1..(iterations + warmup_iterations)).map { |i| [ ACTIONS.sample, sentence, RNG.rand() ] }
 
+user = User.offset(user_offset).first
+unless user
+  print "No user at offset #{user_offset.inspect}! Exiting.\n"
+  exit -1
+end
+
+log "Simulating activity for user id #{user.id}: #{user.name}"
+
+log "Getting Rails CSRF token..."
+client = DiscourseClient.new
+client.get_csrf_token
+
+log "Logging in as #{user.username.inspect}..."
+client.request :post, "/session", { "login" => user.username, "password" => "password" }
+client.request :post, "/login", { "login" => user.username, "password" => "password", "redirect" => "http://localhost:#{PORT_NUM}/" }
+
 (iterations + warmup_iterations).times do |i|
-  DiscourseClient.action_from_args *actions[i]
+  client.action_from_args *actions[i]
 end
