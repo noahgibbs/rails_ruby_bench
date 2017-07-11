bacb254f8e0fd6c3069784c6c7da0d4f52cb51f1
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-07-11 12:34:51 -0700

    Strip whitespace (and newline) from 'rvm current' output.

--- start.rb
+++ start.rb
@@ -216,7 +216,7 @@ test_data = {
   },
   "environment" => {
     "RUBY_VERSION" => RUBY_VERSION,
-    "rvm current" => `rvm current`,
+    "rvm current" => `rvm current`.strip,
   },
   "startup" => {
     "times" => startup_times
