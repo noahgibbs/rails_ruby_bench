ed4e628a65d0e9d4016cf0e39a047804cab96ca5
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-07-11 12:16:13 -0700

    WIP: work on multiply-usable templating script for graphical output.

--- graph/rickshaw.rb
+++ graph/rickshaw.rb
@@ -3,26 +3,38 @@
 require "json"
 require "erubis"
 
-RICKSHAW_TEMPLATE_PATH = File.join(__dir__, "rickshaw_template.html.erb")
-RICKSHAW_TEMPLATE = File.read RICKSHAW_TEMPLATE_PATH
+templates = Dir["*.html.erb"].to_a
 
-OUTPUT_FILE = "rickshaw_graph.html"
+if ARGV.include?["-t"]
+  idx = ARGV.index("-t")
+  ARGV.delete(idx)
+  templates = ARGV.delete(idx).split(",")
+end
 
 raise "Wrong number of arguments!" if ARGV.size != 1
 input_data = JSON.load File.read(ARGV[0])
 
-er = Erubis::Eruby.new(RICKSHAW_TEMPLATE)
+output_path = nil # Set the scope for this local
 
-output = er.result :data => input_data
+templates.each do |t|
+  output_path = File.basename(t) + ".html"
 
-File.open(OUTPUT_FILE, "w") do |f|
-  f.print output
+  er = Erubis::Eruby.new File.read(t)
+
+  output = er.result :data => input_data
+
+  File.open(output_path, "w") do |f|
+    f.print output
+  end
 end
 
-print "Opening #{OUTPUT_FILE.inspect} in Chrome..."
-system "open -a \"Google Chrome.app\" #{OUTPUT_FILE}"
-if $?.success?
-  print " (Succeeded!)\n"
-else
-  print " (Failed! #{$?.to_i})\n"
+# One template? Open the result in Chrome
+if templates.size == 1
+  print "Opening #{output_path.inspect} in Chrome..."
+  system "open -a \"Google Chrome.app\" #{OUTPUT_FILE}"
+  if $?.success?
+    print " (Succeeded!)\n"
+  else
+    print " (Failed! #{$?.to_i})\n"
+  end
 end
