0fd42a8d953d954424f7b21b325aef02bbcc1ab2
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-09-19 16:06:25 -0700

    WIP Docker: Various fixes to test script.

--- docker/test_benchmark_run.rb
+++ docker/test_benchmark_run.rb
@@ -2,27 +2,24 @@
 
 RAILS_BENCH_DIR = "/var/rails_ruby_bench"
 
+class DockerBuildError < RuntimeError; end
+
 # Checked system - error if the command fails
 def csystem(cmd, err, opts = {})
   cmd = "bash -l -c \"#{cmd}\"" if opts[:bash]
-  print "Running command: #{cmd.inspect}\n" if VERBOSE || opts[:debug] || opts["debug"]
-  if VERBOSE
-    system(cmd, out: $stdout, err: :out)
-  else
-    out = `#{cmd}`
-  end
+  print "Running command: #{cmd.inspect}\n" if opts[:debug] || opts["debug"]
+  system(cmd, out: $stdout, err: :out)
   unless $?.success? || opts[:fail_ok] || opts["fail_ok"]
     puts "Error running command:\n#{cmd.inspect}"
-    puts "Output:\n#{out}\n=====" if out
-    raise SystemPackerBuildError.new(err)
+    raise DockerBuildError.new(err)
   end
 end
 
 # And check to make sure the benchmark actually runs... But just do a few iterations.
 Dir.chdir(RAILS_BENCH_DIR) do
   begin
-    csystem "rvm use #{first_ruby} && ./start.rb -s 1 -n 1 -i 10 -w 0 -o /tmp/ -c 1", "Couldn't successfully run the benchmark!", :bash => true
-  rescue SystemPackerBuildError
+    csystem "./start.rb -s 1 -n 1 -i 10 -w 0 -o /tmp/ -c 1", "Couldn't successfully run the benchmark!", :bash => true
+  rescue DockerBuildError
     # Before dying, let's look at that Rails logfile... Redirect stdout to stderr.
     print "Error running test iterations of the benchmark, printing Rails log to console!\n==========\n"
     print `tail -60 work/discourse/log/profile.log`   # If we echo too many lines they just get cut off by Packer
