636ab637a2cb724b318d148e1e30d8be2f5babb9
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-10-09 15:09:08 -0700

    Bash wrapper for bench_start.sh

--- docker/bench_start.sh
+++ docker/bench_start.sh
@@ -1,1 +1,1 @@
-sudo -H -u discourse "cd /var/rails_ruby_bench && ./start.rb --no-startup-shutdown -p 3000 $<"
+sudo -H -u discourse bash -c "cd /var/rails_ruby_bench && ./start.rb --no-startup-shutdown -p 3000 \"$@\""
