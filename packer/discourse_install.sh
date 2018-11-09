f4acc3ac97cd3a065106fe1d5f0fbee1576e2aa0
Noah Gibbs <the.codefolio.guy@gmail.com>
2018-11-09 12:12:29 -0600

    Fix postgres packages installed on Bionic

--- packer/discourse_install.sh
+++ packer/discourse_install.sh
@@ -20,16 +20,13 @@ sudo apt-get update
 whoami > /tmp/username
 sudo add-apt-repository ppa:chris-lea/redis-server
 sudo apt-get -yqq update
-sudo apt-get -yqq --allow-unauthenticated install vim curl expect debconf-utils git-core build-essential zlib1g-dev libssl-dev openssl libcurl4-openssl-dev libreadline6-dev libpcre3 libpcre3-dev imagemagick redis-server advancecomp gifsicle jhead jpegoptim libjpeg-turbo-progs optipng pngcrush pngquant gnupg2
+sudo apt-get -yqq --allow-unauthenticated install vim curl expect debconf-utils git-core build-essential zlib1g-dev libssl-dev openssl libcurl4-openssl-dev libreadline6-dev libpcre3 libpcre3-dev imagemagick redis-server advancecomp gifsicle jhead jpegoptim libjpeg-turbo-progs optipng pngcrush pngquant gnupg2 postgresql postgresql-contrib postgresql-client libpq-dev
 #sudo apt-get -yqq --allow-unauthenticated install python-software-properties vim curl expect debconf-utils git-core build-essential zlib1g-dev libssl-dev openssl libcurl4-openssl-dev libreadline6-dev libpcre3 libpcre3-dev imagemagick postgresql-server postgresql-contrib libpq-dev redis-server advancecomp gifsicle jhead jpegoptim libjpeg-turbo-progs optipng pngcrush pngquant gnupg2
 
 # Ruby
 curl -sSL https://rvm.io/mpapis.asc | gpg2 --import -
 curl -sSL https://get.rvm.io | bash -s stable
 echo 'gem: --no-document' >> ~/.gemrc
-
-# Postgres prep
-sudo adduser postgres  # Needed on Ubuntu Bionic where no 'postgres' user is created
 
 # Node
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
