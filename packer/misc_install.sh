0ae77f867a45d01f6618647f3adea201f0f93081
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-01-08 10:24:47 -0800

    Revert "Add JDK7 to support JMeter 3+"
    This reverts commit b409ca207bb5aa219c5bdf63ad9f611b22dfcb63.

--- packer/misc_install.sh
+++ packer/misc_install.sh
@@ -4,6 +4,5 @@ set -e
 
 # To be run as "ubuntu" user w/ sudo access
 
-# Install OpenJDK-headless for Java 7 to run JMeter
+sudo apt-get -yqq install apache2-utils libjemalloc-dev libtcmalloc-minimal4
 
-sudo apt-get -yqq install apache2-utils libjemalloc-dev libtcmalloc-minimal4 openjdk-7-jre-headless
