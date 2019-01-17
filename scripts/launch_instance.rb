bc2efc942013494fba6f65047afd841807feb37a
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-01-17 15:23:25 -0800

    Write script commands to a file under home from instance-launch script

--- scripts/launch_instance.rb
+++ scripts/launch_instance.rb
@@ -2,6 +2,9 @@
 
 require "json"
 
+# While you might get some useful ideas here, this script is *not* general-purpose
+# and will *not* do exactly what you wish it would. It's pretty specific to my workflow.
+
 json_out = `aws ec2 run-instances --count 1 --instance-type m4.2xlarge --key-name noah-packer-1 --placement Tenancy=dedicated --image-id ami-086666f4fee1175fb --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=RailsRubyBenchTestInstance}]'`
 
 ec2_info = JSON.parse(json_out)
@@ -13,7 +16,11 @@ ec2_info["Instances"].each do |instance|
     inst_ip = `aws ec2 describe-instances --instance-ids #{id} --query 'Reservations[*].Instances[*].PublicIpAddress' --output text`.strip
 
     puts "Launched EC2 instance: #{ec2_type.inspect} / #{id.inspect}"
-    puts "ssh -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}"
-    puts "scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rails_ruby_bench/data/*.json ."
-    puts "scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rsb/data/*.json ."
+    cmd_lines = <<LINES
+ssh -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}
+scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rails_ruby_bench/data/*.json .
+scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rsb/data/*.json .
+LINES
+    puts cmd_lines
+    FILE.open("~/rrb_commands.txt", "w") { |f| f.puts(cmd_lines) }
 end
