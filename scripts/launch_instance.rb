0216619f44fa14e668686ab61cc924dc20ce2950
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-01-24 11:26:51 -0800

    Update AMI to latest built

--- scripts/launch_instance.rb
+++ scripts/launch_instance.rb
@@ -5,7 +5,7 @@ require "json"
 # While you might get some useful ideas here, this script is *not* general-purpose
 # and will *not* do exactly what you wish it would. It's pretty specific to my workflow.
 
-json_out = `aws ec2 run-instances --count 1 --instance-type m4.2xlarge --key-name noah-packer-1 --placement Tenancy=dedicated --image-id ami-086666f4fee1175fb --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=RailsRubyBenchTestInstance}]'`
+json_out = `aws ec2 run-instances --count 1 --instance-type m4.2xlarge --key-name noah-packer-1 --placement Tenancy=dedicated --image-id ami-00247b9bfae81953c --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=RailsRubyBenchTestInstance}]'`
 
 ec2_info = JSON.parse(json_out)
 
@@ -20,6 +20,7 @@ ec2_info["Instances"].each do |instance|
 ssh -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}
 scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rails_ruby_bench/data/*.json .
 scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rsb/data/*.json .
+aws ec2 terminate-instances --instance-ids #{id}
 LINES
     puts cmd_lines
     File.open("~/rrb_commands.txt", "w") { |f| f.puts(cmd_lines) }
