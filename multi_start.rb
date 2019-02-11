2dd9dc7b3ff56244bbba5446ed8d746ce2b136b0
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-02-11 15:14:44 -0800

    Remove obsolete start script

--- multi_start.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-
-# Run start.rb with the specified arguments in all the marked-as-installed Rubies in the Rails Ruby Bench installation.
-
-RUBIES = File.read("/home/ubuntu/benchmark_ruby_versions.txt").split("\n")
-
-RUBIES.each do |ruby|
-  system("bash -l -c \"rvm use #{ruby} && ./start.rb #{ARGV.join(" ")}\"")
-end
