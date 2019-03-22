0da43ba3ab96167441764596e5c8724382836f0c
Noah Gibbs <the.codefolio.guy@gmail.com>
2019-03-21 17:02:48 -1000

    Update w/ latest AMI, add JRuby to Packer images by default

--- scripts/launch_instance.rb
+++ scripts/launch_instance.rb
@@ -5,13 +5,14 @@ require "json"
 # While you might get some useful ideas here, this script is *not* general-purpose
 # and will *not* do exactly what you wish it would. It's pretty specific to my workflow.
 
-latest_ami = 'ami-0b2a485a58d6120c7'
+latest_ami = 'ami-0b255be9ed8d6f7cc'
 inst_name = ENV['INSTANCE_NAME'] || 'RailsRubyBenchTestInstance'
 inst_type = ENV['INSTANCE_TYPE'] || 'm4.2xlarge'
 json_out = `aws ec2 run-instances --count 1 --instance-type #{inst_type} --key-name noah-packer-1 --placement Tenancy=dedicated --image-id #{latest_ami} --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=#{inst_name}}]'`
 
 ec2_info = JSON.parse(json_out)
 
+# This is really not a sensible way to handle multiple instances (which I don't use currently)
 ec2_info["Instances"].each do |instance|
     id = instance["InstanceId"]
     ec2_type = instance["InstanceType"]
@@ -24,6 +25,8 @@ ssh -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}
 scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rails_ruby_bench/data/*.json .
 scp -i ~/.ssh/noah-packer-1.pem ubuntu@#{inst_ip}:~/rsb/data/*.json .
 aws ec2 terminate-instances --instance-ids #{id}
+
+AppFolio only: assume-role appfolio-dev User MFA_TOKEN
 LINES
     puts cmd_lines
     File.open("#{ENV["HOME"]}/rrb_commands.txt", "w") { |f| f.puts(cmd_lines) }
