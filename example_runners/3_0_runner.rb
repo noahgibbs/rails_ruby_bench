b3e6c51827ba2b57f1fbb2fb78646bca5ea1ee1a
Noah Gibbs <the.codefolio.guy@gmail.com>
2020-10-08 12:46:35 +0100

    Preserve the Ruby 3.0.0-preview1 runner

---
+++ example_runners/3_0_runner.rb
@@ -0,0 +1,58 @@
+#!/usr/bin/env ruby
+
+# Simple example runner script, editable for different uses
+
+RUBIES = [
+  "2.5.3",
+  "2.6.6",
+  "2.7.0",
+  "2.7.1",
+  "3.0.0-preview1",
+]
+
+TESTS = [
+  "gem install bundler -v 1.17.3 && bundle _1.17.3_ && bundle _1.17.3_ exec ./start.rb -i 15000 -w 1000 -s 0 --no-warm-start -o data/",
+]
+
+TIMES = 30
+
+# Some potentially useful snippets
+
+WITH_COMPACT="export RUBY_COMPACT=YES && echo GC.compact > ~/rails_ruby_bench/work/discourse/config/initializers/900-gc-compact.rb"
+NO_COMPACT="export RUBY_COMPACT=NO && rm -f ~/rails_ruby_bench/work/discourse/config/initializers/900-gc-compact.rb"
+
+# Checked system - error if the command fails
+def csystem(cmd, err, opts = {})
+  cmd = "bash -l -c \"#{cmd}\"" if opts[:bash] || opts["bash"]
+  print "Running command: #{cmd.inspect}\n" if opts[:to_console] || opts["to_console"] || opts[:debug] || opts["debug"]
+  if opts[:to_console] || opts["to_console"]
+    system(cmd, out: $stdout, err: :out)
+  else
+    out = `#{cmd}`
+  end
+  unless $?.success? || opts[:fail_ok] || opts["fail_ok"]
+    puts "Error running command:\n#{cmd.inspect}"
+    puts "Output:\n#{out}\n=====" if out
+    raise err
+  end
+end
+
+commands = []
+RUBIES.each do |ruby|
+  TESTS.each_with_index do |test, test_index|
+    if ruby["2.7.0"] || ruby["3.0"]
+      jit_invocation = "rvm use #{ruby} && export RRB_WITH_JIT=YES && export RUBY_RUNNER_TEST_INDEX=#{test_index} && #{test}"
+      no_jit_invocation = "rvm use #{ruby} && export RRB_WITH_JIT=YES && export RUBY_RUNNER_TEST_INDEX=#{test_index} && #{test}"
+      commands.concat([jit_invocation, no_jit_invocation] * TIMES)
+    else
+      invocation = "rvm use #{ruby} && export RUBY_RUNNER_TEST_INDEX=#{test_index} && #{test}"
+      commands.concat([invocation] * TIMES)
+    end
+  end
+end
+
+rand_commands = commands.shuffle
+
+rand_commands.each do |command|
+  csystem(command, "Error running test!", bash: true, to_console: true)
+end
