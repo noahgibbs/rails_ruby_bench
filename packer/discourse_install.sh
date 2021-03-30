2b505b80928f7d1c7da7ad578e638e1ecd1f463e
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-30 15:54:52 +0100

    Continuing forward with debugging the script

--- packer/discourse_install.sh
+++ packer/discourse_install.sh
@@ -4,6 +4,7 @@ set -e
 
 sudo apt-get update -y
 
+# See: https://meta.discourse.org/t/beginners-guide-to-install-discourse-on-ubuntu-for-development/14727
 bash <(wget -qO- https://raw.githubusercontent.com/techAPJ/install-rails/master/linux)
 
 # It's used by Discourse asset build, but not installed by the script above
--- packer/misc_install.sh
+++ packer/misc_install.sh
@@ -6,15 +6,15 @@ set -e
 
 sudo apt-get -yqq install apache2-utils libjemalloc-dev libtcmalloc-minimal4 openjdk-8-jdk
 
-# Passenger install for comparing app servers
-sudo apt-get install -y dirmngr gnupg
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install -y apt-transport-https ca-certificates
-
-sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
-sudo apt-get update
-
-sudo apt-get install -y passenger
+## Passenger install for comparing app servers
+#sudo apt-get install -y dirmngr gnupg
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 561F9B9CAC40B2F7
+#sudo apt-get install -y apt-transport-https ca-certificates
+#
+#sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger xenial main > /etc/apt/sources.list.d/passenger.list'
+#sudo apt-get update
+#
+#sudo apt-get install -y passenger
 
 # Wrk
 cd /home/ubuntu
