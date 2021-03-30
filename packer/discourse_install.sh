53a9c705b7b19bd7b683b80c5046744c785e55b0
Noah Gibbs <the.codefolio.guy@gmail.com>
2021-03-30 15:29:08 +0100

    More debugging as the script moves forward...

--- packer/discourse_install.sh
+++ packer/discourse_install.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-sudo apt-get update
+sudo apt-get update -y
 
 bash <(wget -qO- https://raw.githubusercontent.com/techAPJ/install-rails/master/linux)
 
-
+# It's used by Discourse asset build, but not installed by the script above
+sudo apt-get install brotli -y
